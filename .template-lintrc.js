39ace4292942b96dc3f181e8df99d51fb9c774db
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-01-05 15:03:54 +0100

    correct errors to make eslint / ember-template-lint happy

---
+++ .template-lintrc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+  rules: {
+    'block-indentation': false,
+    'attribute-indentation': false,
+    'no-html-comments': false,
+    'quotes': false
+  }
+};
--- app/mixins/confirmation-modal-controller.js
+++ app/mixins/confirmation-modal-controller.js
@@ -1,10 +1,11 @@
 import Mixin from '@ember/object/mixin';
+import { htmlSafe } from '@ember/string';
 
 export default Mixin.create({
   actions: {
     showConfirmation: function(title, text, action) {
       this.set('confirmationTitle', title);
-      this.set('confirmationText', text);
+      this.set('confirmationText', htmlSafe(text));
       this.set('confirmationAction', action);
 
       this.send('showModal', 'confirmation-modal', null, this);

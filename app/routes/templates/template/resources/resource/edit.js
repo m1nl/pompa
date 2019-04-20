fc2d0d14cb9f9bf58045cc09bd85d8d1ffe5968c
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-04-20 19:53:27 +0200

    add action panel to resource edit/delete templates

--- app/routes/templates/template/resources/resource/edit.js
+++ app/routes/templates/template/resources/resource/edit.js
@@ -4,6 +4,12 @@ export default Route.extend({
   controllerName: 'resources.resource.edit',
   renderTemplate: function(controller, model) {
     this.render('templates.template.resources.index');
+    this.render('templates.template.resources.action-panel', {
+      into: 'templates.template',
+      outlet: 'action-panel',
+      model: this.modelFor('templates.template.resources.index'),
+      controller: this.controllerFor('templates.template.resources.index'),
+    });
     this.render('resources.resource.edit', {
       into: 'application',
       outlet: 'modal',

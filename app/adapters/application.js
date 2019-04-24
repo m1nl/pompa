1173662f38039d74e95ef84d461114c1858d4286
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-04-25 01:36:32 +0200

    add handler for 202 accepted responses

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,24 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import ENV from "../config/environment";
+import { isBlank } from '@ember/utils';
 
 export default ActiveModelAdapter.extend({
   namespace: ENV.APP.apiNamespace,
   host: ENV.APP.apiHost,
+  handleResponse: function(status, headers, payload, requestData) {
+    if (status === 202 && payload['status'] === 'pending') {
+      let host = this.get('host');
+      let namespace = this.get('namespace');
+
+      if (!host || host === '/') {
+        host = '';
+      }
+
+      let url = payload['tracking']['url'];
+
+      return this.ajax(`${host}${url}?sync=true`);
+    }
+
+    return this._super(...arguments);
+  },
 });

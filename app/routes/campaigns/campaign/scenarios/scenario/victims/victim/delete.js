ea8ad9cbb88d209aaae7cb4fffc116c5a296b079
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-04-30 13:22:32 +0200

    correct victim delete route

--- app/routes/campaigns/campaign/scenarios/scenario/victims/victim/delete.js
+++ app/routes/campaigns/campaign/scenarios/scenario/victims/victim/delete.js
@@ -1,12 +1,18 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  renderTemplate: function(controller, model) {
+  controllerName: 'victims.victim.delete',
+  renderTemplate: function() {
     this.render('campaigns.campaign.scenarios.scenario.index');
+    this.render('campaigns.campaign.scenarios.scenario.action-panel', {
+      into: 'campaigns.campaign',
+      outlet: 'action-panel',
+      model: this.modelFor('campaigns.campaign.scenarios.scenario'),
+      controller: this.controllerFor('campaigns.campaign.scenarios.scenario.index'),
+    });
     this.render('victims.victim.delete', {
       into: 'application',
       outlet: 'modal',
-      model: model,
     });
   },
   model: function() {

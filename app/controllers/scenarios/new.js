cb47c55b2ee206eb2f61ed93c1523d4ccb334c34
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-05-29 23:54:59 +0200

    reload campaign when scenario is modified

--- app/controllers/scenarios/new.js
+++ app/controllers/scenarios/new.js
@@ -2,4 +2,9 @@ import Controller from '@ember/controller';
 import CreateModalController from 'pompa/mixins/create-modal-controller';
 
 export default Controller.extend(CreateModalController, {
+  afterSave: function(deferred) {
+    return this.model.belongsTo('campaign').reload().then(function() {
+      deferred.resolve();
+    });
+  },
 });
--- app/controllers/scenarios/scenario/edit.js
+++ app/controllers/scenarios/scenario/edit.js
@@ -2,4 +2,9 @@ import Controller from '@ember/controller';
 import EditModalController from 'pompa/mixins/edit-modal-controller';
 
 export default Controller.extend(EditModalController, {
+  afterSave: function(deferred) {
+    return this.model.belongsTo('campaign').reload().then(function() {
+      deferred.resolve();
+    });
+  },
 });

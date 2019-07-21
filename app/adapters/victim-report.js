4b229a8cfbf14cb202fc757555a5656175868655
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-07-21 23:38:49 +0200

    add missing adapters

---
+++ app/adapters/victim-report.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import ApplicationAdapter from 'pompa/adapters/application';
+
+export default ApplicationAdapter.extend({
+  pathForType: function() {
+    return 'victims';
+  },
+  buildURL: function() {
+    let value = this._super(...arguments);
+
+    return `${value}/report`;
+  },
+});

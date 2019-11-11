0939cf573efc7aa5b2782306fb68b1a80d951585
Mateusz Nalewajski <mateusz@nalewajski.pl>
2019-11-11 22:21:53 +0100

    minor layout changes

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,4 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  /* properties */
+  tagName: 'nav',
+  classNames: ['navbar', 'navbar-inverse', 'navbar-fixed-top'],
 });

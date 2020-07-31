8713a349685514a07a4bbe7cdafc2cbed9bea031
Mateusz Nalewajski <mateusz@nalewajski.pl>
2020-07-31 12:24:08 +0200

    better error handling

--- app/controllers/targets/upload.js
+++ app/controllers/targets/upload.js
@@ -1,6 +1,7 @@
 import { isNone } from '@ember/utils';
 import { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
+import { camelize, capitalize } from '@ember/string';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
@@ -26,8 +27,20 @@ export default Controller.extend({
       adapter.upload(this.file, this.params).then(function() {
         deferred.resolve();
       }, function(response) {
-        if (response.body && response.body.errors && response.body.errors.file) {
-          let errors = response.body.errors.file.map((v) => { return { message: v } });
+        if (response.body && response.body.errors) {
+          let errors = []
+
+          Object.keys(response.body.errors).forEach(function(p) {
+            let property = response.body.errors[p];
+
+            if (Array.isArray(property)) {
+              property.forEach(function(v) {
+                let message = capitalize(camelize(p)) + ": " + v;
+                errors.push({ message: message });
+              });
+            }
+          });
+
           self.set('errors', errors);
         }
         deferred.reject();
